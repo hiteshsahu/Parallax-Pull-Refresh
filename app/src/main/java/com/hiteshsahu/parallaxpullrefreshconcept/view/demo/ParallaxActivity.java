a843c9dac9206f8f332a008e337be2aa9355ab8c
hiteshsahu <hiteshkumarsahu1990@gmail.com>
2016-10-06 21:43:09 +0530

    Removed useless classes and optimized for performance rearrange packages

--- app/src/main/java/com/hiteshsahu/parallaxpullrefreshconcept/view/demo/ParallaxActivity.java
+++ app/src/main/java/com/hiteshsahu/parallaxpullrefreshconcept/view/demo/ParallaxActivity.java
@@ -6,7 +6,6 @@ import android.annotation.TargetApi;
 import android.graphics.Path;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
@@ -38,54 +37,35 @@ public class ParallaxActivity extends AppCompatActivity {
     private static final double VERTICAL_DISPLACEMNT = 150.0;
     private static final int TRAJECTORY_DURATION = 2000;//2000;
     @BindView(R.id.parallax_header)
-    ParallaxNestedScrollView scrollView;
-    @BindView(R.id.mountain_bg)
-    ImageView mountain;
-    @BindView(R.id.sky_bg)
-    ImageView sky;
-    @BindView(R.id.coconut_bg)
-    ImageView tree;
-    @BindView(R.id.fab)
-    FloatingActionButton fab;
-    @BindView(R.id.testData)
-    RecyclerView recycler;
-    @BindView(R.id.parent)
+    ParallaxNestedScrollView parallaxedPullRefreshView;
+    @BindView(R.id.root)
     ViewGroup parentViewGroup;
-    int[] img_coordinates = new int[2];
+    //Parallax Layers
+    @BindView(R.id.parallax_layer_bottom)
+    ImageView parallaxLayer1;
+    @BindView(R.id.parallax_layer_intermediate)
+    ImageView parallaxLayer2;
+    @BindView(R.id.parallax_layer_top)
+    ImageView parallaxLayer3;
+    //ImageView drone
+    @BindView(R.id.drone)
+    ImageView drone;
+    //Recyclerview for demo
+    @BindView(R.id.demo_recycle_view)
+    RecyclerView recycler;
     private String TAG = ParallaxActivity.class.getSimpleName();
     private int currentGfCount = 0;
     private GlideDrawableImageViewTarget imageViewTarget;
-    private String[] urls = {
-            //Add Sky
-            "http://wallpapercave.com/wp/0e12dgE.png",
-            "http://wallpapercave.com/wp/3I32MKI.jpg",
-            "http://wallpapercave.com/wp/251fACI.jpg",
-            "http://wallpapercave.com/wp/kKsNXp9.jpg",
-            "http://wallpapercave.com/wp/jGNTLTN.jpg",
-            "http://wallpapercave.com/wp/HfzLYZY.jpg",
-            "http://wallpapercave.com/wp/2l8fNqv.jpg",
-            "http://wallpapercave.com/wp/gF9n5Lk.jpg",
-            "http://wallpapercave.com/wp/cRHOcKE.jpg",
-            "http://wallpapercave.com/wp/KBk1YBX.jpg",
-            //Tree
-            "http://img03.deviantart.net/7245/i/2013/134/3/c/tree_2__png_with_transparency__by_bupaje-d65amxg.png",
-            "http://img04.deviantart.net/95e3/i/2013/136/e/a/tree_3__png_with_transparency__by_bupaje-d65gvf3.png",
-            "http://img07.deviantart.net/38e2/i/2013/135/b/2/tree_1__png_with_transparency__by_bupaje-d65ctod.png",
-            //Hills
-            "http://www.pngall.com/wp-content/uploads/2016/06/Mountain-PNG-Picture.png",
-            "http://www.pngall.com/wp-content/uploads/2016/06/Mountain-PNG-File.png",
-            "http://david-freed.com/wp-content/uploads/2014/04/Slider-Mountain-1440-wide-1.png",
-            "http://pre02.deviantart.net/0627/th/pre/f/2013/218/c/8/mountain_landscape_by_regus_ttef-d602k3h.png",
-            "http://www.pngall.com/wp-content/uploads/2016/06/Mountain-PNG-Image-180x180.png"
-    };
+    private int[] droneLaunchCoordinates = new int[2];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parallax);
-
         ButterKnife.bind(this);
 
+        //This app uses images from internet to avoid huge apk size and souce size
         Snackbar.make(parentViewGroup, "This App Download and Cache Images from INTERNET Consider turning WIFI On for better experience", Snackbar.LENGTH_LONG)
                 .setAction("OK", null).show();
 
@@ -100,29 +80,41 @@ public class ParallaxActivity extends AppCompatActivity {
                             parentViewGroup.getViewTreeObserver().removeGlobalOnLayoutListener(this);
                         }
 
-                        // measure your views here
-                        fab.getLocationOnScreen(img_coordinates);
+                        // find drone coordinates
+                        drone.getLocationOnScreen(droneLaunchCoordinates);
                     }
                 }
         );
 
+        //fill data for app
         TestDataProvider.getInstance().generateTestData();
 
-        // Apply over-scroll in 'overshoot mode' - i.e. using the helper.
-        IOverScrollDecor decor = new VerticalOverScrollBounceEffectDecorator(new NestedScrollViewOverScrollDecorAdapter(scrollView),
+        //you can use Gifs on first layer of parallax it looks awesome
+        imageViewTarget = new GlideDrawableImageViewTarget(parallaxLayer1);
+
+        //add images on parallax layers
+        Glide.with(getApplicationContext()).load(R.drawable.night_sky).into(parallaxLayer1);
+        Glide.with(getApplicationContext()).load(R.drawable.back_mountain_transparent_bg).into(parallaxLayer2);
+        Glide.with(getApplicationContext()).load(R.drawable.coconut).into(parallaxLayer3);
+
+        // Apply over-scroll in 'overshoot mode'  for nested scrollview
+        IOverScrollDecor overScrollDecor = new VerticalOverScrollBounceEffectDecorator(new NestedScrollViewOverScrollDecorAdapter(parallaxedPullRefreshView),
                 VerticalOverScrollBounceEffectDecorator.DEFAULT_TOUCH_DRAG_MOVE_RATIO_FWD,
                 VerticalOverScrollBounceEffectDecorator.DEFAULT_TOUCH_DRAG_MOVE_RATIO_BCK,
                 -1f);
 
         // Over-scroll listeners can be applied in standard form as well.
-        decor.setOverScrollUpdateListener(new IOverScrollUpdateListener() {
+        overScrollDecor.setOverScrollUpdateListener(new IOverScrollUpdateListener() {
             @Override
             public void onOverScrollUpdate(IOverScrollDecor decor, int state, float offset) {
-                Log.e(TAG, "Scroll By" + String.valueOf((int) offset));
+                Log.e(TAG, "Scrolled By" + String.valueOf((int) offset));
 
                 if (offset > 0) {
                     // 'view' is currently being over-scrolled from the top.
-                    fab.setRotation(-offset);
+
+                    //convert Linear Motion of overscroll into
+                    // Rotation motion of drone
+                    drone.setRotation(-offset);
 
 //                    calculateHorizontalDisplacemnet = HORIZONTAL_DISPLACEMENT * Math.sin(offset );
 
@@ -137,7 +129,7 @@ public class ParallaxActivity extends AppCompatActivity {
             }
         });
 
-        decor.setOverScrollStateListener(new IOverScrollStateListener() {
+        overScrollDecor.setOverScrollStateListener(new IOverScrollStateListener() {
             @Override
             public void onOverScrollStateChange(IOverScrollDecor decor, int oldState, int newState) {
                 switch (newState) {
@@ -151,16 +143,18 @@ public class ParallaxActivity extends AppCompatActivity {
                         // Dragging started at the right-end.
                         break;
                     case IOverScrollState.STATE_BOUNCE_BACK:
+
+                        // Dragging stopped -- view is starting to bounce back from the *left-end* onto natural position.
                         if (oldState == IOverScrollState.STATE_DRAG_START_SIDE) {
 
                             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-                                moveit(fab);
+                                shootDrone(drone);
                             } else {
-                                moveItPreLollipop();
+                                shootDronePreLollipop();
                             }
 
-                            // Dragging stopped -- view is starting to bounce back from the *left-end* onto natural position.
-                        } else { // i.e. (oldState == STATE_DRAG_END_SIDE)
+                        } else {
+                            // i.e. (oldState == STATE_DRAG_END_SIDE)
                             // View is starting to bounce back from the *right-end*.
                         }
                         break;
@@ -168,13 +162,8 @@ public class ParallaxActivity extends AppCompatActivity {
             }
         });
 
-        Glide.with(getApplicationContext()).load(R.drawable.night_sky).into(sky);
-        Glide.with(getApplicationContext()).load(R.drawable.back_mountain_transparent_bg).into(mountain);
-        Glide.with(getApplicationContext()).load(R.drawable.coconut).into(tree);
-
-        imageViewTarget = new GlideDrawableImageViewTarget(sky);
-
-        fab.setOnClickListener(new View.OnClickListener() {
+        //change background of parallax layer 1
+        drone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -200,9 +189,9 @@ public class ParallaxActivity extends AppCompatActivity {
             }
         });
 
-        recycler.setHasFixedSize(true);
+        //setup recycle view
+        recycler.setHasFixedSize(false);
         recycler.setNestedScrollingEnabled(false);
-
         recycler.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
         TestDataAdapter rcAdapter = new TestDataAdapter();
         recycler.setAdapter(rcAdapter);
@@ -211,11 +200,26 @@ public class ParallaxActivity extends AppCompatActivity {
             public void OnCardClicked(View view, int position) {
 
                 if (position <= 9) {
-                    Glide.with(getApplicationContext()).load(urls[position]).into(sky);
+
+                    //first 9 items are skys images , update sky layer
+                    Glide.with(getApplicationContext())
+                            .load(TestDataProvider.testImageUrls[position])
+                            .placeholder(R.drawable.night_sky)
+                            .into(parallaxLayer1);
+
                 } else if (position > 9 && position < 13) {
-                    Glide.with(getApplicationContext()).load(urls[position]).into(tree);
+
+                    //next3 items are trees , update tree layer
+                    Glide.with(getApplicationContext())
+                            .load(TestDataProvider.testImageUrls[position])
+                            .placeholder(R.drawable.coconut)
+                            .into(parallaxLayer3);
                 } else {
-                    Glide.with(getApplicationContext()).load(urls[position]).into(mountain);
+                    //other items are mountains , update mountain layer
+                    Glide.with(getApplicationContext())
+                            .load(TestDataProvider.testImageUrls[position])
+                            .placeholder(R.drawable.back_mountain_transparent_bg)
+                            .into(parallaxLayer2);
                 }
             }
         });
@@ -226,19 +230,19 @@ public class ParallaxActivity extends AppCompatActivity {
      *
      * @param view
      */
-
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    private void moveit(final View view) {
+    private void shootDrone(final View view) {
 
         float x = view.getX();
         float y = view.getY();
         Path path = new Path();
 
+        //TODO refine motion
         path.moveTo(x + 0, y + 0);
         path.quadTo(x + 0, y + 0, x + 1000, y - 500);
         path.quadTo(x + 1000, y - 500, x - 300, y - 500);
         path.quadTo(x - 300, y - 500, x - 300, y - 200);
-        path.quadTo(x - 300, y - 500, img_coordinates[0], img_coordinates[1]);
+        path.quadTo(x - 300, y - 500, droneLaunchCoordinates[0], droneLaunchCoordinates[1]);
         ObjectAnimator objectAnimator =
                 null;
 
@@ -251,7 +255,7 @@ public class ParallaxActivity extends AppCompatActivity {
     /**
      * Animate Shoot for pre Lollipop and lower Android
      */
-    private void moveItPreLollipop() {
+    private void shootDronePreLollipop() {
         final ValueAnimator shootAnimation = ValueAnimator.ofFloat(0, 1); // values from 0 to 1
         shootAnimation.setDuration(TRAJECTORY_DURATION); // 1 seconds duration from 0 to 1
         shootAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
@@ -265,19 +269,20 @@ public class ParallaxActivity extends AppCompatActivity {
                 Log.e(TAG, " update position" + value);
                 // Set translation of your view here. Position can be calculated
                 // out of value. This code should move the view in a half circle.
-                fab.setTranslationX((float) (HORIZONTAL_DISPLACEMENT * Math.sin(value * Math.PI)));
+                drone.setTranslationX((float) (HORIZONTAL_DISPLACEMENT * Math.sin(value * Math.PI)));
 
                 //increase height till half the trajectory
                 if (value < .5) {
-                    fab.setTranslationY((float) (VERTICAL_DISPLACEMNT * Math.cos(value)));
+                    drone.setTranslationY((float) (VERTICAL_DISPLACEMNT * Math.cos(value)));
                 } else {
                     //decrase for rest
-                    fab.setTranslationY((float) ((VERTICAL_DISPLACEMNT - 150.0 * value) * Math.cos(value)));
+                    drone.setTranslationY((float) ((VERTICAL_DISPLACEMNT - 150.0 * value) * Math.cos(value)));
                 }
             }
         });
 
         shootAnimation.start();
     }
+
 
 }
